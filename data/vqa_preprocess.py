4812f8c2749f939417171a6b00f79f990b41837e
rshivansh <33856562+rshivansh@users.noreply.github.com>
2017-12-08 10:37:46 +0000

    Update vqa_preprocess.py

--- data/vqa_preprocess.py
+++ data/vqa_preprocess.py
@@ -1,1 +1,42 @@
+"""
+Download the vqa data and preprocessing.
+Version: 1.0
+Contributor: Jiasen Lu
+"""
 
+# Download the VQA Questions from http://www.visualqa.org/download.html
+import json
+import os 
+import argparse
+
+def download_vqa():
+ 
+  # Downlaod the VQA Questions :
+  os.system('wget http://visualqa.org/data/mscoco/vqa/Questions_Train_mscoco.zip -P zip/')
+  os.system('wget http://visualqa.org/data/mscoco/vqa/Questions_Val_mscoco.zip -P zip/')
+  os.system('wget http://visualqa.org/data/mscoco/vqa/Questions_Test_mscoco.zip -P zip/')
+  
+  # Download the VQA Annotations :
+  os.system('wget http://visualqa.org/data/mscoco/vqa/Annotations_Train_mscoco.zip -P zip/')
+  os.system('wget http://visualqa.org/data/mscoco/vqa/Annotations_Val_mscoco.zip -P zip/')
+  
+  # Let us now unzip the annotations :
+  os.system('unzip zip/Questions_Train_mscoco.zip -d annotations/')
+  os.system('unzip zip/Questions_Val_mscoco.zip -d annotations/')
+  os.system('unzip zip/Questions_Test_mscoco.zip -d annotations/')
+  os.system('unzip zip/Annotations_Train_mscoco.zip -d annotations/')
+  os.system('unzip zip/Annotations_Val_mscoco.zip -d annotations/')
+  
+  def main(params):
+    if params['downlaod'] == 1:
+      download_vqa 
+      
+      # Note the following :
+    '''
+    Put the VQA data into single  JavaScript Object Notation or JSON file, where [[Question_id, Image_id, Question, multipleChoice_answer, Answer] ... ]
+    '''
+    
+    
+
+
+  
