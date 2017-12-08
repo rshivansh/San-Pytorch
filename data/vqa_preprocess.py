db5ebaa62d03e4b301999a5ba80a8293d748d56c
rshivansh <33856562+rshivansh@users.noreply.github.com>
2017-12-08 12:31:29 +0000

    Update vqa_preprocess.py

--- data/vqa_preprocess.py
+++ data/vqa_preprocess.py
@@ -11,7 +11,7 @@ import argparse
 
 def download_vqa():
  
-  # Downlaod the VQA Questions :
+  
   os.system('wget http://visualqa.org/data/mscoco/vqa/Questions_Train_mscoco.zip -P zip/')
   os.system('wget http://visualqa.org/data/mscoco/vqa/Questions_Val_mscoco.zip -P zip/')
   os.system('wget http://visualqa.org/data/mscoco/vqa/Questions_Test_mscoco.zip -P zip/')
@@ -28,14 +28,107 @@ def download_vqa():
   os.system('unzip zip/Annotations_Val_mscoco.zip -d annotations/')
   
   def main(params):
-    if params['downlaod'] == 1:
-      download_vqa 
+      if params['download'] == 1:
+          download_vqa()
+
+            '''
+            Put the VQA data into single JavaScript Object Notation or json file, where [[Question_id, Image_id, Question, multipleChoice_answer, Answer] ... ]
+            '''
+
+      train = []
+      test = []
+      imdir='%s/COCO_%s_%012d.jpg'
       
-      # Note the following :
-    '''
-    Put the VQA data into single  JavaScript Object Notation or JSON file, where [[Question_id, Image_id, Question, multipleChoice_answer, Answer] ... ]
-    '''
-    
+      if params['split'] == 1:
+       # Train on train and Test on val
+          print 'Loading annotations and questions...'
+          train_anno = json.load(open('annotations/mscoco_train2014_annotations.json', 'r'))
+          val_anno = json.load(open('annotations/mscoco_val2014_annotations.json', 'r'))
+
+          train_ques = json.load(open('annotations/MultipleChoice_mscoco_train2014_questions.json', 'r'))
+          val_ques = json.load(open('annotations/MultipleChoice_mscoco_val2014_questions.json', 'r')) 
+           
+          subtype = 'train2014'
+          for i in range(len(train_anno['annotations'])):
+              ans = train_anno['annotations'][i]['multiple_choice_answer']
+              question_id = train_anno['annotations'][i]['question_id']
+              image_path = imdir%(subtype, subtype, train_anno['annotations'][i]['image_id'])
+
+              question = train_ques['questions'][i]['question']
+              mc_ans = train_ques['questions'][i]['multiple_choices']
+              train.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'MC_ans': mc_ans, 'ans': ans})
+            
+          subtype = 'val2014'
+          for i in range(len(val_anno['annotations'])):
+              ans = val_anno['annotations'][i]['multiple_choice_answer']
+              question_id = val_anno['annotations'][i]['question_id']
+              image_path = imdir%(subtype, subtype, val_anno['annotations'][i]['image_id'])
+
+              question = val_ques['questions'][i]['question']
+              mc_ans = val_ques['questions'][i]['multiple_choices']  
+              test.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'MC_ans': mc_ans, 'ans': ans})
+              
+      else :
+          # train on train +val and Test on test
+          print " Loading annotations and questions "
+          train_anno = json.load(open('annotations/mscoco_train2014_annotations.json', 'r'))
+          val_anno = json.load(open('annotations/mscoco_val2014_annotations.json', 'r'))
+
+          train_ques = json.load(open('annotations/MultipleChoice_mscoco_train2014_questions.json', 'r'))
+          val_ques = json.load(open('annotations/MultipleChoice_mscoco_val2014_questions.json', 'r'))
+          test_ques = json.load(open('annotations/MultipleChoice_mscoco_test2015_questions.json', 'r'))
+        
+          subtype = 'train2014'
+          for i in range(len(train_anno['annotations'])):
+              ans = train_anno['annotations'][i]['multiple_choice_answer']
+              question_id = train_anno['annotations'][i]['question_id']
+              image_path = imdir%(subtype, subtype, train_anno['annotations'][i]['image_id'])
+
+              question = train_ques['questions'][i]['question']
+              mc_ans = train_ques['questions'][i]['multiple_choices']
+
+              train.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'MC_ans': mc_ans, 'ans': ans})
+
+          subtype = 'val2014'
+          for i in range(len(val_anno['annotations'])):
+              ans = val_anno['annotations'][i]['multiple_choice_answer']
+              question_id = val_anno['annotations'][i]['question_id']
+              image_path = imdir%(subtype, subtype, val_anno['annotations'][i]['image_id'])
+
+              question = val_ques['questions'][i]['question']
+              mc_ans = val_ques['questions'][i]['multiple_choices']
+
+              train.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'MC_ans': mc_ans, 'ans': ans})
+        
+          subtype = 'test2015'
+          for i in range(len(test_ques['questions'])):
+              question_id = test_ques['questions'][i]['question_id']
+              image_path = imdir%(subtype, subtype, test_ques['questions'][i]['image_id'])
+
+              question = test_ques['questions'][i]['question']
+              mc_ans = test_ques['questions'][i]['multiple_choices']
+
+              test.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'MC_ans': mc_ans})
+          
+      print 'Training sample %d, Testing sample %d...' %(len(train), len(test))
+
+      json.dump(train, open('vqa_raw_train.json', 'w'))
+      json.dump(test, open('vqa_raw_test.json', 'w'))
+
+  if __name__ == "__main__":
+
+      parser = argparse.ArgumentParser()
+
+      # input json
+      parser.add_argument('--download', default=1, type=int, help='Download and extract data from VQA server')
+      parser.add_argument('--split', default=1, type=int, help='1: train on Train and test on Val, 2: train on Train+Val and test on Test')
+  
+      args = parser.parse_args()
+      params = vars(args)
+      print 'parsed input parameters:'
+      print json.dumps(params, indent = 2)
+  main(params)
+      
     
 
 
